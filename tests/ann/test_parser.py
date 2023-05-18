1b212a4c57b7ece08b7a9380eb82495fda8dafad
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-05-17 22:48:18 -0400

    Fix type errors

--- tests/ann/test_parser.py
+++ tests/ann/test_parser.py
@@ -1,7 +1,7 @@
 from charybdis.ann import parser, types
 
 
-def test_parse_ann():
+def test_parse_ann() -> None:
     ann = types.Ann(
         addr=types.BankAddr(bank=0x01, addr=0x1234), label="Test", type=None
     )
